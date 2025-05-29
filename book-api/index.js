ceba2804fa75b39d389db7479c2e91e695fa20ad
data-detectiv <oppongyaw049@gmail.com>
2025-05-29 01:34:12 -0400

    feat: index.js updated

--- book-api/index.js
+++ book-api/index.js
@@ -1,1 +1,9 @@
 const express = require('express');
+const app = express()
+const uuid = require('uuid');
+const PORT = process.env.PORT || 3500;
+
+
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+
