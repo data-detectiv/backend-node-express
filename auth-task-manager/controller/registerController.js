3d4f12475397f039f727288d8e38c26fef222a13
data-detectiv <oppongyaw049@gmail.com>
2025-05-31 23:18:14 -0400

    feat: register route created

---
+++ auth-task-manager/controller/registerController.js
@@ -0,0 +1,34 @@
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) {this.users = data}
+}
+
+const bcrypt = require('bcrypt');
+const fsPromises = require('fs').promises;
+const path = require('path')
+
+const handleNewUser = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.'});
+    const duplicate = usersDB.users.find(person => person.username === user)        ;
+    if (duplicate) return res.sendStatus(409);
+
+    try {
+        const hashedpwd = await bcrypt.hash(pwd, 10);
+        const newUser = {
+            "username": user,
+            "password": hashedpwd
+        };
+        usersDB.setUsers([...usersDB.users, newUser]);
+        await fsPromises.writeFile(
+            path.join(__dirname, '..', 'model', 'users.json'),
+            JSON.stringify(usersDB.users)
+        );
+
+    } catch (err) {
+        res.status(500).json({ "message":`${err.message}`});
+    }
+    res.json({...usersDB.users});
+};
+
+module.exports = { handleNewUser };
--- auth-task-manager/server.js
+++ auth-task-manager/server.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3500;
 
+app.use(express.json());
+
+app.use('/register', require('./routes/register'));
+
+app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
